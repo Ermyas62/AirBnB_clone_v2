28dbe43c4a38c3c3509862f7571f55991d3624a3
Ermyas62 <ermyas62@gmail.com>
2024-03-19 02:23:57 -0700

    user

--- models/user.py
+++ models/user.py
@@ -5,15 +5,16 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
-from models.temp import HBNB_TYPE_STORAGE, DB
 
 
 class User(BaseModel, Base):
     """
     This class define user by various attributes
     """
+
     __tablename__ = 'users'
-    if getenv(HBNB_TYPE_STORAGE) == DB:
+
+    if getenv("HBNB_TYPE_STORAGE") == "DB":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
